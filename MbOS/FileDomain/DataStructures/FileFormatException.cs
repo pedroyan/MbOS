f64eabf06696ff70c16c55f749007863d897bdbc
Pedro Yan Ornelas de Oliveira <pedroyane@gmail.com>
2017-10-11 06:10:00 -0300

    Basic exception handling

---
+++ MbOS/FileDomain/DataStructures/FileFormatException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MbOS.FileDomain.DataStructures {
+	[Serializable]
+	internal class FileFormatException : Exception {
+		public FileFormatException() {
+		}
+
+		public FileFormatException(string message) : base(message) {
+		}
+
+		public FileFormatException(string message, Exception innerException) : base(message, innerException) {
+		}
+
+		protected FileFormatException(SerializationInfo info, StreamingContext context) : base(info, context) {
+		}
+	}
+}
--- MbOS/FileDomain/FileManager.cs
+++ MbOS/FileDomain/FileManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MbOS.FileDomain.DataStructures;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
@@ -23,16 +24,28 @@ namespace MbOS.FileDomain {
 				InitializeHDD(file);
 			} catch (FileNotFoundException) {
 				Console.WriteLine($"Erro ao ler arquivo: arquivo não encontrado no path de execução {location}");
+				throw;
 			} catch (DirectoryNotFoundException) {
 				Console.WriteLine($"Diretório do arquivo inválido: {location}");
+				throw;
+			} catch(FileFormatException ex) {
+				Console.WriteLine($"Arquivo inválido: {ex.Message}");
+				throw;
 			} catch (Exception ex) {
-				Console.WriteLine($"Erro ao ler arquivo{ex.Message}");
+				Console.WriteLine($"Erro ao ler arquivo: {ex.Message}");
+				throw;
 			}
 		}
 
 		private void InitializeHDD(StreamReader reader) {
 			var line = reader.ReadLine();
-			if (!int.TryParse(reader.ReadLine(), out int hdSize)) {
+			if (!int.TryParse(line, out int hdSize)) {
+				throw new FileFormatException($@"A primeira linha deve conter um valor numérico representando o tamanho do HDD. \n Valor encontrado:{line}");
+			}
+
+			line = reader.ReadLine();
+			if (!int.TryParse(line,out int initializationListSize)) {
+				throw new FileFormatException($@"A segunda linha deve conter um valor numérico representando o tamanho da lista de inicialização. \n Valor encontrado:{line}");
 			}
 		}
 	}
